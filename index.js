bf191e25cc69508d42d9b2733aada26ef081d2ab
Ike Saunders <saunders.ike@gmail.com>
2018-09-30 18:30:44 -0400

    prettier

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-const express = require('express')
-const path = require('path')
-const reload = require('reload')
-const app = express()
-reload(app)
+const express = require("express");
+const path = require("path");
+const reload = require("reload");
+const app = express();
+reload(app);
 
-const project = process.argv[2]
+const project = process.argv[2];
 
-app.get('/', (req, res) => res.sendFile(path.join(__dirname + '/index.html')))
-app.use('/js', express.static(`sketches/${project}`))
-app.use(express.static(__dirname))
-app.listen(3000, () => console.log(`Example app listening on port 3000`))
+app.get("/", (req, res) => res.sendFile(path.join(__dirname + "/index.html")));
+app.use("/js", express.static(`sketches/${project}`));
+app.use(express.static(__dirname));
+app.listen(3000, () => console.log(`Example app listening on port 3000`));
