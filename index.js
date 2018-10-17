e4252f126b3edaebef09f6a1a76eb3e40b2fc95d
Ike Saunders <saunders.ike@gmail.com>
2018-10-17 09:22:40 -0400

    look: eye cursor tracking

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
-const express = require("express");
-const path = require("path");
-const reload = require("reload");
+const express = require('express');
+const path = require('path');
+const reload = require('reload');
 const app = express();
 reload(app);
 
 const project = process.argv[2];
-
-app.get("/", (req, res) => res.sendFile(path.join(__dirname + "/index.html")));
-app.use("/js", express.static(`sketches/${project}`));
+app.get('/', (req, res) => res.sendFile(path.join(__dirname + '/index.html')));
+app.use('/js', express.static(`sketches/${project}`));
 app.use(express.static(__dirname));
 app.listen(3000, () => console.log(`Example app listening on port 3000`));
--- sketches/look/index.js
+++ sketches/look/index.js
@@ -1,30 +1,35 @@
+const height = 400;
+const width = 600;
+const halfHeight = height / 2;
+const halfWidth = width / 2;
+const radius = 100;
 function setup() {
-  createCanvas(windowWidth, windowHeight - 4);
+  createCanvas(width, height);
   colorMode(HSB);
+  angleMode(DEGREES);
   noStroke();
   rectMode(CENTER);
-  angleMode(DEGREES);
 }
 
 function draw() {
   background(0);
+  const center = createVector(halfWidth, halfHeight);
+  const mouse = createVector(mouseX, mouseY);
+
+  fill(0, 0, 100);
+  arc(center.x, center.y - 100, 400, 400, 30, 150, CHORD);
+  arc(center.x, center.y + 100, 400, 400, 210, 330, CHORD);
+
   fill(255);
-  ellipse(windowWidth / 2, windowHeight / 2, 200);
-  fill(100, 100, 50);
-  const yc = windowHeight / 2;
-  const xc = windowWidth / 2;
-  const dy = yc - 0.2 * (yc - mouseY);
-  const dx = xc - 0.2 * (xc - mouseX);
-  const distanceX = mouseX - xc;
-  const distanceY = -1 * (mouseY - yc);
-  const theta = atan(distanceY / distanceX);
-  console.log("x", distanceX);
-  // console.log("y", distanceY);
-  // console.log("theta: ", theta);
-  const cVector = {
-    y: -sin(theta) * 100,
-    x: cos(theta) * 100
-  };
-    console.log('vector x', cVector.x)
-  rect(cVector.x + xc, cVector.y + yc, 50, 50);
+  ellipse(center.x, center.y, 2 * radius);
+
+  const diff = p5.Vector.sub(mouse, center);
+  const mag = diff.mag();
+  const distance = constrain(mag, 0, 150);
+  diff.normalize();
+  diff.mult((distance / 150) * (radius - 10));
+  translate(halfWidth, halfHeight);
+
+  fill(175, 70, 0);
+  ellipse(diff.x, diff.y, 100);
 }
